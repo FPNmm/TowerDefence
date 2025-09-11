f53d0facf4c67f8bf7f05d61c9e2fa47e245d6fd
Frederick <fpinto@stud.macromedia.de>
2025-09-11 15:41:46 +0200

    Tower Firerate

--- Assets/Scripts/TowerController.cs
+++ Assets/Scripts/TowerController.cs
@@ -5,13 +5,26 @@ public class Tower : MonoBehaviour
     public float FireRate = 1.0f;
     public float damage;
 
+    private float cooldown;
+
+    private void Start()
+    {
+        cooldown = FireRate;
+    }
+
+    private void Update()
+    {
+        cooldown -= Time.deltaTime;
+    }
+
     private void OnTriggerStay(Collider other)
     {
         EnemyController enemy = other.GetComponent<EnemyController>();
 
-        if (enemy)
+        if (cooldown <= 0f && enemy)
         {
             enemy.KillEnemy();
+            cooldown = FireRate;
         }
     }
 }
