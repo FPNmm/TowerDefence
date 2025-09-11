ad1fe5f785de00c3e1d0b46485e922c13644aa1f
Frederick <fpinto@stud.macromedia.de>
2025-09-11 13:53:36 +0200

    EnemySpawnerCounter
    Counts to 10 only

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -11,6 +11,29 @@ public class EnemySpawner : MonoBehaviour
     private void Start()
     {
         spawnTime = spawnInterval;
+
+        int singleDidgit = 1;
+        int singleDidgit2 = 0;
+        while(singleDidgit2+singleDidgit < 10)
+        {
+            singleDidgit2=singleDidgit2+singleDidgit;
+            if (singleDidgit2 <= 10)
+            {
+                Debug.Log(singleDidgit2);
+            }
+        }
+
+        int numberOfEnemiesSpawned = 0;
+        while (numberOfEnemiesSpawned < waveSize)
+        {
+            SpawnEnemy();
+            numberOfEnemiesSpawned++;
+        }
+
+        for(int numOfEnemiesSpawned = 0; numOfEnemiesSpawned < waveSize; numOfEnemiesSpawned++)
+        {
+            SpawnEnemy();
+        }
     }
 
     private void Update()
@@ -22,5 +45,10 @@ public class EnemySpawner : MonoBehaviour
     private void SpawnEnemy()
     {
         Instantiate(enemyPrefab, transform.position, Quaternion.identity, transform);
+    }
+
+    private int AddNumbers(int num1, int num2)
+    {
+        return num1 + num2;
     }
 }
